2cc5c6a432f94e9f5a59446b7a13a6109dd39b05
Hide <padocon@naver.com>
2022-12-01 13:15:21 +0900

    Implement redis config

---
+++ src/main/kotlin/boilerplate/sample/core/config/RedisConfig.kt
@@ -0,0 +1,50 @@
+package boilerplate.sample.core.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.cache.CacheManager
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import org.springframework.data.redis.cache.RedisCacheConfiguration
+import org.springframework.data.redis.cache.RedisCacheManager
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.RedisSerializationContext
+import org.springframework.data.redis.serializer.StringRedisSerializer
+import java.time.Duration
+
+@Configuration
+class RedisConfig(
+    @Value("\${spring.redis.host}")
+    private val host: String,
+
+    @Value("\${spring.redis.port}")
+    private val port: Int,
+) {
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory {
+        return LettuceConnectionFactory(host, port)
+    }
+
+    @Primary
+    @Bean(name = ["redisCacheManager"])
+    fun redisCacheManager(
+        redisConnectionFactory: RedisConnectionFactory,
+    ): CacheManager {
+        val redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+            .serializeKeysWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(StringRedisSerializer())
+            )
+            .serializeValuesWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(GenericJackson2JsonRedisSerializer())
+            )
+        return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(redisConnectionFactory)
+            .cacheDefaults(redisCacheConfiguration)
+            .withCacheConfiguration(
+                "redis-cache",
+                RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(5))
+            )
+            .build()
+    }
+}
