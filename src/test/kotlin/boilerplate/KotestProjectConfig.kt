a65e70989c17d38ac20f58080ce866cbc9d5a68a
Hide <padocon@naver.com>
2022-12-05 22:42:31 +0900

    Add KotestProjectConfig

---
+++ src/test/kotlin/boilerplate/KotestProjectConfig.kt
@@ -0,0 +1,9 @@
+package boilerplate
+
+import io.kotest.core.config.AbstractProjectConfig
+import io.kotest.core.extensions.Extension
+import io.kotest.extensions.spring.SpringExtension
+
+class KotestProjectConfig : AbstractProjectConfig() {
+    override fun extensions(): List<Extension> = listOf(SpringExtension)
+}
